030d0ce8f1a596ca36696be5c4ec960ca3b80204
Burak <iletisim@drementer.com>
2021-11-21 13:41:05 +0300

    js modullere ayrılıyor ve fonksiyon ekleniyor

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+export function overflow_kapat(){
+    document.querySelector("html, body").style.overflowY = "hidden";
+}
+
+export function overflow_ac(){
+    document.querySelector("html, body").style.overflowY = "auto";
+}
