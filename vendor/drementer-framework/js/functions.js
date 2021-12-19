9771e845b1e168ee55a09b1b073e37130d5d230b
Burak <iletisim@drementer.com>
2021-12-19 07:34:47 +0300

    her_slider component eklendi

--- vendor/drementer-framework/js/functions.js
+++ vendor/drementer-framework/js/functions.js
@@ -5,3 +5,22 @@ export function scrool_kapat(){
 export function scrool_ac(){
     document.querySelector("html, body").style.overflowY = "auto";
 }
+
+export function hero_slider(){
+    let yazi_sayisi = 5;
+    var hero_slider = document.querySelectorAll("div.__hero-slider");
+    for(let i = 0; i < hero_slider.length; i++){
+        let item = hero_slider[i];
+        let yazi = document.createElement("div");
+        yazi.classList.add("__yazi");
+        yazi.innerHTML = item.dataset.yazi;
+        for(let i = 1; i<= yazi_sayisi; i++){
+            item.innerHTML += "";
+            item.appendChild(yazi);
+        }
+    }
+}
+
+export function app(){
+    hero_slider();
+}
