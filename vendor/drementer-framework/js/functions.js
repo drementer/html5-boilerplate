e75d4c07c8ddc2d0ad36e565b8e4c518a9550336
Burak <iletisim@drementer.com>
2021-12-20 10:40:33 +0300

    hero slider güncellendi ve yorum satırları eklendi

--- vendor/drementer-framework/js/functions.js
+++ vendor/drementer-framework/js/functions.js
@@ -1,3 +1,4 @@
+/* Genel */
 export function scrool_kapat(){
     document.querySelector("html, body").style.overflowY = "hidden";
 }
@@ -5,22 +6,40 @@ export function scrool_kapat(){
 export function scrool_ac(){
     document.querySelector("html, body").style.overflowY = "auto";
 }
+/* Genel SON */
 
+
+/* Hero Slider oluşturucu */
 export function hero_slider(){
-    let yazi_sayisi = 5;
     var hero_slider = document.querySelectorAll("div.__hero-slider");
-    for(let i = 0; i < hero_slider.length; i++){
-        let item = hero_slider[i];
+    hero_slider.forEach(item => {
+
+        // Oluşturulacak alt eleman
+        let yazi_sayisi = item.dataset.sayi;
+
+        // Minimum değerin altında giriş yapıldığında
+        if(yazi_sayisi < 5 || yazi_sayisi == undefined){
+            console.warn("Hero Slider yazi sayisi minimum 5 olabilir, bu yüzden otomatik olarak 5 ayarlandı.");
+            yazi_sayisi = 5;
+        }
+
+        // Component oluşturuluyor
         let yazi = document.createElement("div");
+        // Oluşturulan componente .__yazi class eklendi
         yazi.classList.add("__yazi");
+        // Oluşturulan componente ebeveyine girilen data-yazi attr si içerik olarak eklendi
         yazi.innerHTML = item.dataset.yazi;
+        // Ebeveyine girilen data-sayi attr kadar ebeveyine oluşturulan component eklendi
         for(let i = 1; i<= yazi_sayisi; i++){
             item.innerHTML += "";
             item.appendChild(yazi);
         }
-    }
+    })
 }
+/* Hero Slider oluşturucu SON */
 
+
+// Girilen fonksiyonlar varsayılan olarak dışa atıldı
 export function app(){
     hero_slider();
 }
