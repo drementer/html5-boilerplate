e801ebfae066297385e182a73506571ef20145f4
Burak <48869708+drementer@users.noreply.github.com>
2022-04-28 10:02:29 +0300

    assets dred v1.3.0 ile güncellendi

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,21 @@
 /* 
     by drementer
     klncarslanburak@gmail.com
-    iletisim@drementer.com
 	
 	MIT LICENSE: (https://github.com/drementer/dred/blob/master/LICENSE)
 */
 
-import * as fonksiyon from './functions.js';
-fonksiyon.app();
+/* İmport */
+import {
+	scroll_ac,
+	scroll_kapat,
+	log,
+	is_string,
+	is_null,
+	is_number,
+	gtt,
+	is_dark_mode,
+} from "./functions.js";
+import cerez from "./cerez_popup.js";
+cerez();
+/* İmport SON */
--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,95 +1,45 @@
 /* Genel */
-export let scrool_kapat = () => {
-	document.querySelector("html, body").style.overflowY = "hidden";
-};
-
-export let scrool_ac = () => {
-	document.querySelector("html, body").style.overflowY = "auto";
-};
+let scroll_kapat = () => {
+		document.querySelector("html, body").style.overflowY = "hidden";
+	},
+	scroll_ac = () => {
+		document.querySelector("html, body").style.overflowY = "auto";
+	},
+	log = console.log.bind(document),
+	is_string = (value) => {
+		return typeof value === "string" || value instanceof String;
+	},
+	is_null = (value) => {
+		return value !== null || value !== undefined;
+	},
+	is_number = (value) => {
+		return !isNaN(value);
+	},
+	gtt = () => window.scrollTo(0, 0),
+	is_dark_mode =
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches;
 /* Genel SON */
 
-/* Hero Slider oluşturucu */
-let hero_slider = () => {
-	const hero_slider = document.querySelectorAll("div.__hero-slider");
-	hero_slider.forEach((slider) => {
-		// Oluşturulacak alt eleman
-		let yazi_sayisi = slider.dataset.yaziSayisi;
+/*  */
+///		(function () {
+///			some private code that will be executed automatically
+///		})();
+///		(function (a, b) {
+///			var result = a + b;
+///			return result;
+///		})(10, 20);
+/*  */
 
-		// Minimum değerin altında giriş yapıldığında
-		if (yazi_sayisi < 5 || yazi_sayisi == undefined) {
-			console.warn(
-				"Hero Slider yazi sayisi minimum 5 olabilir, bu yüzden otomatik olarak 5 ayarlandı."
-			);
-			yazi_sayisi = 5;
-		}
-
-		// Yaziları kapsayacak div oluşturuldu
-		const hero_icerik = document.createElement("div");
-		hero_icerik.classList.add("__icerik");
-
-		// Yazilar için component oluşturuldu
-		const yazi = document.createElement("div");
-		yazi.classList.add("__yazi");
-		yazi.innerHTML = slider.innerHTML;
-		slider.innerHTML = null;
-
-		// Ebeveyine kapsayici eklendi
-		slider.appendChild(hero_icerik);
-		// Oluşturulacak alt eleman
-
-		// Kapsayıcıya istenilen miktarda yazi eklendi
-		for (let i = 1; i <= yazi_sayisi; i++) {
-			hero_icerik.insertAdjacentHTML("beforeend", yazi.outerHTML);
-		}
-	});
+/* Export */
+export {
+	scroll_ac,
+	scroll_kapat,
+	log,
+	is_string,
+	is_null,
+	is_number,
+	gtt,
+	is_dark_mode,
 };
-/* Hero Slider oluşturucu SON */
-
-/* Çerez Politikası ve Popup */
-let cerez_popup = () => {
-	// Atamalar
-	let cerez_politikasi__isim = "cerez-politikasi",
-		cerez_politikasi__deger = localStorage.getItem(cerez_politikasi__isim),
-		cerez_popup = document.getElementById("cerez-popup"),
-		cerez_popup__tuslar = cerez_popup.querySelectorAll(
-			".__butonlar > .buton"
-		),
-		cerez_popup__kapat = () => {
-			if (!cerez_popup.matches(".--kapali")) {
-				cerez_popup.classList.add("--kapali");
-			}
-		},
-		cerez_kabul_et = () => {
-			localStorage.setItem(cerez_politikasi__isim, "kabul");
-			cerez_popup__kapat();
-		};
-
-	cerez_popup__tuslar.forEach((tus) => {
-		tus.addEventListener("click", () => {
-			if (tus.matches(".--kabul")) {
-				cerez_kabul_et();
-			} else {
-				cerez_popup__kapat();
-			}
-		});
-	});
-	// Tarayıcı çerezlerinin aktif olup olmadığına bakıyor
-	if (window.localStorage) {
-		if (cerez_politikasi__deger) {
-			if (cerez_politikasi__deger === "kabul") {
-				cerez_popup__kapat();
-			}
-		}
-	} else {
-		console.error("Taraycının çerez desteği kapalı, lütfen aktive edin!");
-	}
-};
-/* Çerez Politikası ve Popup SON */
-
-// Girilen fonksiyonları başlangıçta çalıştırmak üzere dışarı aktarır
-export var app = () => {
-	document.addEventListener("DOMContentLoaded", () => {
-		cerez_popup();
-		hero_slider();
-	});
-};
+/* Export SON */
