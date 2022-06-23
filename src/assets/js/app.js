1f1bcf103ced91cf0d680bbae1c937f68477f4b9
Burak <48869708+drementer@users.noreply.github.com>
2022-06-23 22:33:38 +0300

    anlamsız klasörler ve dosyalar temizlendi

--- src/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-/*!
- * @author drementer
- * klncarslanburak@gmail.com
- * @license MIT (https://github.com/drementer/proje-ismi/blob/master/LICENSE)
-*/
-
-/* Atamalar */
-const doc = document,
-	body = doc.body;
